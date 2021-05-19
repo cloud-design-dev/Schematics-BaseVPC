ee4ead962f0a668f4baf4e49d39749729ca031e0
greyhoundforty <ryantiffany@gmail.com>
2021-05-19 16:20:59 -0500

    Fixes #1
    Signed-off-by: greyhoundforty <ryantiffany@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,16 @@
-# output "ssh_command" {
-#   value       = "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p root@${var.create_public_ip ? module.vpc-bastion.bastion_public_ip : module.vpc-bastion.bastion_private_ip}' root@${ibm_is_instance.instance.primary_network_interface[0].primary_ipv4_address}"
-#   description = "Command to use to jump from the bastion to the instance assuming you are using your own default SSH key on bastion and instances"
-# }
+output "ssh_command" {
+  value       = "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p root@${var.create_public_ip ? module.vpc-bastion.bastion_public_ip : module.vpc-bastion.bastion_private_ip}' root@VPC_PRIVATE_IP"
+  description = "Command to use to jump from the bastion to the instance assuming you are using your own default SSH key on bastion and instances"
+}
 
 output "vpc_id" {
   value = module.vpc.id
+}
+
+output "public_gateways" {
+  value = module.public_gateway[*].gateway
+}
+
+output "subnet_cidrs" {
+  value = modile.subnet[*].ipv4_cidr_block
 }
