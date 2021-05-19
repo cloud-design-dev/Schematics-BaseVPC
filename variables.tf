c514762307a8df45388c799a1589173531572480
greyhoundforty <ryantiffany@gmail.com>
2021-05-19 11:44:22 -0500

    Added diagram
    Signed-off-by: greyhoundforty <ryantiffany@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-#variable "ibmcloud_api_key" {
-#  type        = string
-#  description = "IBM Cloud API key to create resources"
-#  default     = ""
-#}
+variable "ibmcloud_api_key" {
+ type        = string
+ description = "IBM Cloud API key to create resources"
+ default     = ""
+}
 
 variable "region" {
   type        = string
